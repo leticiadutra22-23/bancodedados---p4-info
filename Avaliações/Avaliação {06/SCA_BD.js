78d96402d5de3f5fa7520f6aed534871894243e1
leticiadutra22-23 <leticiack.ldtra@gmail.com>
2022-11-04 20:55:13 -0300

    changes has been done

--- Avaliações/Avaliação {06/SCA_BD.js
+++ Avaliações/Avaliação {06/SCA_BD.js
@@ -8,27 +8,27 @@ var sqlite3 = require('sqlite3').verbose();
 var data = new sqlite3.Database("SCA_BD.db");
 
 //CREATING NEW TABLES
-db.run(`CREATE TABLE TB_ALUNO (
+data.run(`CREATE TABLE TB_ALUNO (
     id INTEGER NOT NULL,
     name TEXT NOT NULL,
     birth TEXT NOT NULL,
    PRIMARY KEY ("id" AUTOINCREMENT)
 )`);
 
-db.run(`CREATE TABLE IF TB_DISCIPLINA (
+data.run(`CREATE TABLE TB_DISCIPLINA (
     id INTEGER NOT NULL,
     name TEXT NOT NULL,
     PRIMARY KEY ("id" AUTOINCREMENT)
  )`);
 
-db.run(`CREATE TABLE TB_PROFESSOR (
+data.run(`CREATE TABLE TB_PROFESSOR (
    id INTEGER NOT NULL,
    name TEXT NOT NULL,
    birth TEXT NOT NULL,
    PRIMARY KEY ("id" AUTOINCREMENT)
 )`);
 
-db.run(`CREATE TABLE TB_MATRICULA (
+data.run(`CREATE TABLE TB_MATRICULA (
    id INTEGER NOT NULL,
    aluno_id INTEGER NOT NULL,
    disciplina_id INTEGER NOT NULL,
@@ -39,7 +39,7 @@ db.run(`CREATE TABLE TB_MATRICULA (
    FOREIGN KEY ("professor_id") REFERENCES "TB_PROFESSOR"("id")
 )`);
 
-db.run(`CREATE TABLE TB_PROFESSOR_DISCIPLINA (
+data.run(`CREATE TABLE TB_PROFESSOR_DISCIPLINA (
     id INTEGER NOT NULL,
     disciplina_id INTEGER NOT NULL,
     professor_id INTEGER NOT NULL,
@@ -53,32 +53,32 @@ data.run("INSERT INTO TB_ALUNO (name, birth) VALUES (Regianne, 12/10), (Bianca, 
 data.run("INSERT INTO TB_DISCIPLINA (name) VALUES (Banco de Dados), (Redes de Computadores), (Métodos e Ferramentas de Desenvolvimento de Software), (Programação de Dispositivos Móveis)");
 data.run("INSERT INTO TB_PROFESSOR (name, birth) VALUES (Taveira, 14/09), (Wendell, 29/05), (Serra, 20/12), (Hairon, 09/11)");
 data.run("INSERT INTO TB_MATRICULA (aluno_id, disciplina_id, professor_id) VALUES (1, 1, 1), (2, 2, 2), (3, 3, 3), (3, 3, 3), (4, 4, 4)");
-db.run(`INSERT INTO TB_PROFESSOR_DISCIPLINA (disciplina_id, professor_id) VALUES (1, 1), (2, 2), (3, 2), (3, 3)`);
+data.run(`INSERT INTO TB_PROFESSOR_DISCIPLINA (disciplina_id, professor_id) VALUES (1, 1), (2, 2), (3, 2), (3, 3)`);
 
 
 //QUERYING THE DATABASE
-db.all("SELECT * FROM TB_ALUNO", function(err, rows) {
+data.all("SELECT * FROM TB_ALUNO", function(err, rows) {
     rows.forEach(function (row) {
       console.log(row.id + ": " + row.name + row.birth);
     });
    });
 
-db.all("SELECT * FROM TB_PROFESSOR", function(err, rows) {
+data.all("SELECT * FROM TB_PROFESSOR", function(err, rows) {
     rows.forEach(function (row) {
       console.log(row.id + ": " + row.name + row.birth);
     });
    });
   
-db.all("SELECT * FROM TB_DISCIPLINA", function(err, rows) {
+data.all("SELECT * FROM TB_DISCIPLINA", function(err, rows) {
     rows.forEach(function (row) {
       console.log(row.id + ": " + row.name);
     });
    });
   
-db.each("SELECT TB_MATRICULA.id AS matricula_id, TB_ALUNO.name AS aluno_name, TB_PROFESSOR.name AS professor_name, TB_DISCIPLINA.name AS disciplina_name FROM TB_ALUNO, TB_PROFESSOR, TB_DISCIPLINA INNER JOIN TB_MATRICULA ON TB_ALUNO.id = TB_MATRICULA.aluno_id AND TB_DISCIPLINA.id = TB_MATRICULA.disciplina_id AND TB_PROFESSOR.id = TB_MATRICULA.professor_id", function(err, row) {
+data.each("SELECT TB_MATRICULA.id AS matricula_id, TB_ALUNO.name AS aluno_name, TB_PROFESSOR.name AS professor_name, TB_DISCIPLINA.name AS disciplina_name FROM TB_ALUNO, TB_PROFESSOR, TB_DISCIPLINA INNER JOIN TB_MATRICULA ON TB_ALUNO.id = TB_MATRICULA.aluno_id AND TB_DISCIPLINA.id = TB_MATRICULA.disciplina_id AND TB_PROFESSOR.id = TB_MATRICULA.professor_id", function(err, row) {
     console.log(row.matricula_id + ": (" + row.aluno_name + ", " + row.disciplina_name + ", " + row.professor_name + ")");
 });
 
-db.each("SELECT TB_PROFESSOR_DISCIPLINA.id AS profsd_id, TB_PROFESSOR.name AS professor_name, TB_DISCIPLINA.name AS disciplina_name FROM TB_PROFESSOR, TB_DISCIPLINA INNER JOIN TB_PROFESSOR_DISCIPLINA ON TB_PROFESSOR.id = TB_PROFESSOR_DISCIPLINA.professor_id AND TB_DISCIPLINA.id = TB_PROFESSOR_DISCIPLINA.disciplina_id", function(err, row) {
+data.each("SELECT TB_PROFESSOR_DISCIPLINA.id AS profsd_id, TB_PROFESSOR.name AS professor_name, TB_DISCIPLINA.name AS disciplina_name FROM TB_PROFESSOR, TB_DISCIPLINA INNER JOIN TB_PROFESSOR_DISCIPLINA ON TB_PROFESSOR.id = TB_PROFESSOR_DISCIPLINA.professor_id AND TB_DISCIPLINA.id = TB_PROFESSOR_DISCIPLINA.disciplina_id", function(err, row) {
     console.log(row.profsd_id + ": (" + row.professor_name + ", " + row.disciplina_name + ")");
 });
